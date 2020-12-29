bee3448527361c074765ec6dd72839e95971f6a0
Alejandro Segura <asegurpe@gmail.com>
2020-12-29 20:19:23 +0100

    Implemented send socket message

---
+++ WebSockets/01-server/sockets/socket.ts
@@ -0,0 +1,14 @@
+import { Socket } from "socket.io";
+
+export const desconetar = (cliente: Socket) => {
+    cliente.on('disconnect', () => {
+        console.log("Cliente desconectado");
+    });
+}
+
+// Escuchar mensajes
+export const mensaje = (cliente: Socket) => {
+    cliente.on('mensaje', (payload: {de: string, cuerpo: string}) => {
+        console.log("Mensaje recibido", payload);
+    });
+}
---
+++ WebSockets/02-basico/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = {
+  url: environment.wsUrl , options: {}
+};
+
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { FooterComponent } from './components/footer/footer.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
