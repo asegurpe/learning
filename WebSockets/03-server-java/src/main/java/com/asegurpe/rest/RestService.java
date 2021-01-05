bafdad25934c3ddc1579a609b15fc2a4518a407c
Àlex Segura <asegura@on-laser.com>
2021-01-05 12:18:10 +0100

    Fixed leak

--- WebSockets/03-server-java/src/main/java/com/asegurpe/rest/RestService.java
+++ WebSockets/03-server-java/src/main/java/com/asegurpe/rest/RestService.java
@@ -79,19 +79,16 @@ public class RestService {
 	// save uploaded file to new location
 	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) {
 
-	    try {
-	        OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
+	    try (OutputStream out = new FileOutputStream(new File(uploadedFileLocation))) {
 	        int read = 0;
 	        byte[] bytes = new byte[1024];
 
-	        out = new FileOutputStream(new File(uploadedFileLocation));
 	        while ((read = uploadedInputStream.read(bytes)) != -1) {
 	            out.write(bytes, 0, read);
 	        }
 	        out.flush();
 	        out.close();
 	    } catch (IOException e) {
-
 	        e.printStackTrace();
 	    }
 	}
