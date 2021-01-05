e537ac9474b6b5d88064e75b16d9fcc3b899cbb6
Àlex Segura <asegura@on-laser.com>
2021-01-05 12:17:58 +0100

    Comments

--- TelegramBot/telegrambot/src/main/java/com/asegurpe/rest/RestClient.java
+++ TelegramBot/telegrambot/src/main/java/com/asegurpe/rest/RestClient.java
@@ -3,7 +3,6 @@ package com.asegurpe.rest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -74,7 +73,7 @@ public class RestClient {
 			conn.setRequestMethod("POST");
 			conn.setRequestProperty("Content-Type", "application/json");
 
-			String input = "{\"qty\":100,\"name\":\"iPad 4\"}";
+//			String input = "{\"qty\":100,\"name\":\"iPad 4\"}";
 
 //			OutputStream os = conn.getOutputStream();
 //			os.write(input.getBytes());
