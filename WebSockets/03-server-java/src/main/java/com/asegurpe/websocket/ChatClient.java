b4160369f7eb7410ff74594206d881f43b771e01
Àlex Segura <asegura@on-laser.com>
2021-01-29 14:23:19 +0100

    Test streaming

--- WebSockets/03-server-java/src/main/java/com/asegurpe/websocket/ChatClient.java
+++ WebSockets/03-server-java/src/main/java/com/asegurpe/websocket/ChatClient.java
@@ -1,7 +1,7 @@
 package com.asegurpe.websocket;
 
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -19,6 +19,12 @@ import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 import org.imgscalr.Scalr;
+
+import com.onlaser.designer.core.service.impl.CommandCallServiceImpl;
+import com.onlaser.designer.core.utils.ReflectUtils;
+import com.onlaser.designer.visio.socket.CameraClient;
+import com.onlaser.designer.visio.socket.CameraClient.Command;
+import com.onlaser.designer.visio.util.VisioUtils;
 
 @ServerEndpoint(value = "/chat")
 public class ChatClient {
@@ -69,23 +75,46 @@ public class ChatClient {
 				synchronized (connection) {
 					if (connection.session.isOpen()) {
 						if (message.contains("foto")) {
-							File fi = new File("D:\\Temp\\test1_bevel.bmp");
+							int port = 54000;
+							int width = 1280;
+							int height = 960;
+							CommandCallServiceImpl.getInstance().call("taskkill.exe", "/f", "/im", "BaslerService.exe");
+							CommandCallServiceImpl.getInstance().execute("BaslerService.exe", port);
+							CameraClient cc = new CameraClient("localhost", port);
+							cc.open();
+							cc.setSerial("22088739");
+							cc.connect();
+							cc.setParameter(Command.WIDTH, width);
+							cc.setParameter(Command.HEIGHT, height);
 							
-//							100%
-//							byte[] fileContent = Files.readAllBytes(fi.toPath());
-							
-//							RESIZED
-							BufferedImage in = ImageIO.read(fi);
-//							in = resizeImage(in, 200);
-							in = Scalr.resize(in, 200);
-							ByteArrayOutputStream baos = new ByteArrayOutputStream();
-							ImageIO.write(in, "jpg", baos);
-							byte[] fileContent = baos.toByteArray();
-							
-							ByteBuffer buf = ByteBuffer.wrap(fileContent);
-							connection.session.getBasicRemote().sendBinary(buf);
+							for (int j = 0; j < 1000; j++) {
+								BufferedImage bi = null;
+								byte[] fileContent = null;
+								if (message.contains("foto1")) {
+									File fi = new File("D:\\Temp\\test1_bevel.bmp");
+									
+//									100%
+//									byte[] fileContent = Files.readAllBytes(fi.toPath());
+									
+//									RESIZED
+									bi = ImageIO.read(fi);
+//									in = resizeImage(in, 200);
+								} else if (message.contains("foto2")) {
+									byte[] capture = cc.capture((width * height) + 54);
+									VisioUtils.saveImage("D:\\temp\\camera1.bmp", capture);
+//									byte[] capture = cc.capture(1229878);
+									bi = bytesToImage(capture);
+								}
+								bi = Scalr.resize(bi, 200);
+								
+								ByteArrayOutputStream baos = new ByteArrayOutputStream();
+								ImageIO.write(bi, "bmp", baos);
+								fileContent = baos.toByteArray();
+								
+								ByteBuffer buf = ByteBuffer.wrap(fileContent);
+								connection.session.getBasicRemote().sendBinary(buf);
+							}
 						} else {
-							
 							byte[] text = new byte[message.length() + 2];
 							char[] charArray = message.toCharArray();
 							
@@ -102,6 +131,15 @@ public class ChatClient {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	public static BufferedImage bytesToImage(byte[] imageData) {
+		ByteArrayInputStream bais = new ByteArrayInputStream(imageData);
+		try {
+			return ImageIO.read(bais);
+		} catch (IOException e) {
+			throw ReflectUtils.rethrow(e);
 		}
 	}
 	
